48105a0dee6626e250159dca02a3258444ee2e52
Giancarlos Salas <giansalex@gmail.com>
2021-09-27 21:26:38 -0500

    Add logo disperze

--- packages/native-staking/src/App/routes/Login/index.tsx
+++ packages/native-staking/src/App/routes/Login/index.tsx
@@ -1,16 +1,23 @@
 import { Login as LoginDesign } from "@cosmicdapp/design";
+import { Avatar } from "antd";
 import React from "react";
 import { config } from "../../../config";
 import { pathValidators } from "../../paths";
 import cosmWasmLogo from "./assets/cosmWasmLogo.svg";
+import { CenterBox } from "./style";
 
 export function Login(): JSX.Element {
   return (
-    <LoginDesign
-      pathAfterLogin={pathValidators}
-      appName="Juno Staking"
-      appLogo={cosmWasmLogo}
-      config={config}
-    />
+    <>
+      <LoginDesign
+        pathAfterLogin={pathValidators}
+        appName="Juno Staking"
+        appLogo={cosmWasmLogo}
+        config={config}
+      />
+      <CenterBox>
+        <Avatar src="https://avatars.githubusercontent.com/u/71741453?s=200&v=4" /> by Disperze
+      </CenterBox>
+    </>
   );
 }
